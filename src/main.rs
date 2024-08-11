87557b38abca71bac0d2d4c1fbabc4bb38653109
Lit-innosence <lit.azot@i.softbank.jp>
2024-08-12 00:17:49 +0900

    feat: add serde, serde_json

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use rocket::{Build, Rocket, get, routes};
 use utoipa::OpenApi;
 use utoipa_swagger_ui::SwaggerUi;
+use std::{error::Error, fs::File, io::BufReader, path::Path};
+use serde::{Serialize, Deserialize};
 
 #[derive(OpenApi)]
 #[openapi(paths(
@@ -8,11 +10,42 @@ use utoipa_swagger_ui::SwaggerUi;
 ))]
 struct ApiDoc;
 
+// jsonチェック
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct HelloWorld {
+    pub text: String
+}
+
+// jsonデコード
+fn load_json<P: AsRef<Path>>(path: P) -> Result<HelloWorld, Box<dyn Error>> {
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+    let data = serde_json::from_reader(reader)?;
+    Ok(data)
+}
+
 // ヘルスチェックAPI
 #[utoipa::path(context_path = "")]
 #[get("/hello")]
 fn hello() -> &'static str {
     "Hello, world!"
+}
+
+// jsonテスト
+#[utoipa::path(context_path = "")]
+#[get("/test_json")]
+fn test_json() -> String {
+    const FILEPATH: &str= "./src/test.json";
+
+    let data = match load_json(FILEPATH) {
+        Ok(data) => data,
+        Err(err) => {
+            eprintln!("Error: {}", err);
+            return Default::default();
+        }
+    };
+    data.text
 }
 
 
@@ -37,5 +70,6 @@ fn rocket() -> Rocket<Build> {
             SwaggerUi::new("/swagger-ui/<_..>")
                 .url("/api-docs/openapi.json", ApiDoc::openapi()),
         )
+        .mount("/", routes![test_json])
 }
 
