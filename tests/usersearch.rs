3edbd201e789d2c85bdc30812c8f9849f0bfb1be
Lit-innosence <lit.azot@i.softbank.jp>
2024-10-19 18:15:30 +0900

    test: add error case to usersearch test

--- tests/usersearch.rs
+++ tests/usersearch.rs
@@ -342,3 +342,64 @@ async fn name_is_not_requested() {
     assert_eq!(response.status(), Status::Ok);
     assert_eq!(response.into_json::<UserSearchResponce>().await.unwrap(), expected_data);
 }
+
+// 異常系:jwtを所持していない
+#[rocket::async_test]
+async fn jwt_does_not_exist() {
+    //Arrange
+    let client = Client::tracked(rocket()).await.unwrap();
+    let app = App::new();
+
+    let mainuser = &UserInfo{
+            student_id: String::from("4622999"),
+            family_name: String::from("テスト"),
+            given_name: String::from("太郎")
+        };
+    let couser = &UserInfo{
+            student_id: String::from("4622000"),
+            family_name: String::from("テスト"),
+            given_name: String::from("次郎")
+        };
+
+    let studentpair = &PairInfo{
+        main_user: mainuser.clone(),
+        co_user: couser.clone()
+    };
+
+    let assignment = &AssignmentInfo{
+        student_id: String::from("4622999"),
+        locker_id: String::from("2001"),
+    };
+
+    // dbの初期化
+    setup_db(&app).await;
+
+    // student2人をdbに保存
+    match app.student.register(mainuser).await {
+        Ok(_) => {},
+        Err(err) => {panic!("{}", err);},
+    };
+    match app.student.register(couser).await {
+        Ok(_) => {},
+        Err(err) => {panic!("{}", err);},
+    };
+    // studentpairをdbに保存
+    let student_pair = match app.student_pair.register(studentpair).await {
+        Ok(student_pair) => student_pair,
+        Err(err) => {panic!("{}", err);},
+    };
+
+    match app.assignment_record.register(&student_pair, assignment).await {
+        Ok(_) => {},
+        Err(err) => {panic!("{}", err);},
+    }
+
+    dotenv().ok();
+
+
+    // Act
+    let response = client.get("/api/admin/user-search/2024?familyname=%E3%83%86%E3%82%B9%E3%83%88&givenname=%E5%A4%AA%E9%83%8E").dispatch().await;
+
+    // Assert
+    assert_eq!(response.status(), Status::BadRequest);
+}
