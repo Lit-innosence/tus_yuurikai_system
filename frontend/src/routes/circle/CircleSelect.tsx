1d738a1d4b548779bcb3f4f4fe79f78243caac95
Dot-P <metaalice01@gmail.com>
2024-11-05 19:42:50 +0900

    fix: modify link

--- frontend/src/routes/circle/CircleSelect.tsx
+++ frontend/src/routes/circle/CircleSelect.tsx
@@ -3,8 +3,8 @@ import SelectLayout from '../component/PageSelectLayout';
 
 const content = [
     { title: '登録情報更新', route: '/circle/register/update' },
-    { title: '新規登録', route: '/redirect/example' }, // Google Drive Link
-    { title: '公開資料閲覧', route: '/redirect/google' }, // Google Drive Link
+    { title: '新規登録', route: '/redirect/googleform' },
+    { title: '公開資料閲覧', route: '/redirect/onedrive' },
     { title: '登録状況照会', route: '/circle/register/status' },
 ];
 
--- frontend/src/routes/component/Redirect.tsx
+++ frontend/src/routes/component/Redirect.tsx
@@ -12,18 +12,26 @@ const Redirect = () => {
         alignItems: 'center',
         height: '100vh',
         width: '100vw',
-        flexDirection: 'column', 
+        flexDirection: 'column',
+        padding: '0 20px', 
+        textAlign: 'center', 
+    };
+
+    const linkStyle: React.CSSProperties = {
+        fontSize: '24px',
+        wordWrap: 'break-word', 
+        maxWidth: '100%', 
     };
 
     useEffect(() => {
         let url;
 
         switch (path) {
-            case 'example':
-                url = 'https://example.com';
+            case 'googleform':
+                url = 'https://www.google.co.jp/';
                 break;
-            case 'google':
-                url = 'https://www.google.com';
+            case 'onedrive':
+                url = 'https://onedrive.live.com/?authkey=%21AHAUgQpyo9KUOJA&id=7A895461B26453F8%21123&cid=7A895461B26453F8';
                 break;
             default:
                 url = null;
@@ -39,22 +47,22 @@ const Redirect = () => {
 
     return (
         <div style={loadingStyle}>
-        <Typography.Title level={2}>
-            以下のURLにリダイレクトしています、、、
-        </Typography.Title>
-        {redirectUrl ? (
-            <Typography.Link 
-                href={redirectUrl} 
-                style={{ fontSize: '24px' }} 
-            >
-                {redirectUrl}
-            </Typography.Link>
-        ) : (
-            <Typography.Text style={{ fontSize: '24px' }}>
-                指定したURLが見つかりません。
-            </Typography.Text>
-        )}
-    </div>
+            <Typography.Title level={2}>
+                以下のURLにリダイレクトしています、、、
+            </Typography.Title>
+            {redirectUrl ? (
+                <Typography.Link 
+                    href={redirectUrl} 
+                    style={linkStyle}
+                >
+                    {redirectUrl}
+                </Typography.Link>
+            ) : (
+                <Typography.Text style={{ fontSize: '24px' }}>
+                    指定したURLが見つかりません。
+                </Typography.Text>
+            )}
+        </div>
     );
 };
 
