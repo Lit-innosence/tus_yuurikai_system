1704cf475e5f9cfdfc8f3d5df7d02493ec377be0
Dot-P <metaalice01@gmail.com>
2025-02-11 16:23:33 +0900

    feat: new rule for cookie

--- src/adapters/controller/locker.rs
+++ src/adapters/controller/locker.rs
@@ -343,6 +343,7 @@ pub async fn login(request: Json<LoginFormRequest>, jar: &CookieJar<'_>, app: &S
     // 環境変数TOKEN_KEYを取得
     dotenv().ok();
     let key = env::var("TOKEN_KEY").expect("token key must be set.");
+    let domain = env::var("APP_URL").expect("app url must be set.");
 
     // passwordの検証
     if request.password != credential.password {
@@ -353,10 +354,13 @@ pub async fn login(request: Json<LoginFormRequest>, jar: &CookieJar<'_>, app: &S
 
     // cookieを作成
     let cookie = Cookie::build(("token", token))
-        .path("/")
+        .path("/admin")
+        .domain(domain)
+        .max_age(RocketDuration::hours(1))
         .secure(true)
-        .same_site(SameSite::None)
-        .http_only(true);
+        .same_site(SameSite::Strict)
+        .http_only(true)
+        .build();
 
     jar.add(cookie);
 
@@ -368,12 +372,16 @@ pub async fn login(request: Json<LoginFormRequest>, jar: &CookieJar<'_>, app: &S
 #[post("/logout")]
 pub async fn logout(jar: &CookieJar<'_>) -> Status {
 
+    dotenv().ok();
+    let domain = env::var("APP_URL").expect("app url must be set.");
+
     let expired_cookie = Cookie::build(("token", ""))
-        .path("/")
-        .secure(true)
-        .same_site(SameSite::None)
-        .http_only(true)
+        .path("/admin")
+        .domain(domain)
         .max_age(RocketDuration::seconds(0)) // 即無効化する
+        .secure(true)
+        .same_site(SameSite::Strict)
+        .http_only(true)
         .build();
 
     jar.add(expired_cookie);
