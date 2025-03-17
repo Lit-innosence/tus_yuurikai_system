bf4c449a590204116fa60b0439c82eb88e8e51e3
Dot-P <metaalice01@gmail.com>
2025-03-17 22:56:18 +0900

    Add youtube id

--- frontend/src/routes/constants.ts
+++ frontend/src/routes/constants.ts
@@ -1,7 +1,7 @@
 const constants = {
     backendApiEndpoint: 'http://localhost:8000',
     recaptchaSiteKey: '6Lct6tgqAAAAAKzpQuydbN0A3A9ZKtRbUmdxjhll',
-    youtubeVideoId: 'YOUR_YOUTUBE_VIDEO_ID',
+    youtubeVideoId: 'kJh0diD5bi4',
     advertisementUrl: 'https://x.com/yurikai_tus',
 };
 export default constants;
